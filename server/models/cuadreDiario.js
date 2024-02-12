b2e1eb71b04691950b19d5c583f77afe7273556a
Mario-502 <lavcogt@gmail.com>
2024-02-12 12:14:17 -0500

    UPDATE - NEW MODE CUIADRE

--- server/models/cuadreDiario.js
+++ server/models/cuadreDiario.js
@@ -2,16 +2,24 @@ import mongoose from 'mongoose';
 
 const cuadreDiarioSchema = new mongoose.Schema(
   {
-    dateCuadre: {},
-    Montos: [],
+    index: Number,
+    date: {
+      fecha: String,
+      hora: String,
+    },
     cajaInicial: String,
-    cajaFinal: String,
+    Montos: Array,
+    estado: String,
+    margenError: String,
     corte: String,
+    cajaFinal: String,
+    ingresos: Object,
+    egresos: Object,
     notas: [],
+    userID: String,
   },
   { collection: 'CuadreDiario' }
 );
-
 const CuadreDiario = mongoose.model('CuadreDiario', cuadreDiarioSchema);
 
 export default CuadreDiario;
